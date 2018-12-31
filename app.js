e1df2d3d68271aa198ad80c0d8eaa78c87e393ca
Meet Patel <patelmeet59@gmail.com>
2018-12-31 18:43:50 +0530

    Added Genre Selection without much clutter

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //<!--Created by Meet Patel (201801415). All rights reserved :P -->
-
+var currentGenre = localStorage.getItem("genreSelected");
 
 function populate() {
     if(quiz.isEnded()) {
@@ -36,6 +36,7 @@ function showProgress() {
     var element = document.getElementById("questionStatus");
     var y = localStorage.getItem("storedName");
     element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length +  ".&nbsp;&nbsp;" + y + " is playing.";
+    element.innerHTML += "<br>" + "Genre: " + currentGenre;
 };
 
 function showScores() {
@@ -51,21 +52,44 @@ function showScores() {
 };
 
 
-/* var questions = [
+ /* var questions = [
     new Question("Which one is not an object oriented programming language?", ["Java", "C#","C++", "C"], "C"),
     new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
     new Question("There are ____ main components of object oriented programming.", ["1", "6","2", "4"], "4"),
     new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
     new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework")
-]; */
+];  */
 
-var questions = [
+var string1 = "Computer";
+var string2 = "BigBangTheory";
+var string3 = "Politics";
+var questions;
+if(currentGenre === string1){
+questions = [
     new Question("Which one is not an object oriented programming language?", ["Java", "C#","C++", "C"], "C"),
     new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
     new Question("There are ____ main components of object oriented programming.", ["1", "6","2", "4"], "4"),
     new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
     new Question("MVC is a ____.", ["Language", "Library", "Framework", "All"], "Framework")
 ];
-
+}
+else if(currentGenre === string2){
+questions = [
+    new Question("What song does Sheldon require his caretaker to sing to him when he is sick?", ["Soft Kitty", "Baa Baa Black sheep","Despacito", "Dilbar"], "Soft Kitty"),
+    new Question("Which actor plays Sheldon Cooper's role?", ["Johnny Galecki", "Simon Helberg", "Jim Parsons", "Stan Lee"], "Jim Parsons"),
+    new Question("In what year did The Big Bang Theory premiere?", ["2006", "2007","2009", "2011"], "2007"),
+    new Question("What actor on the show has a doctorate in real life?", ["Elon Musk", "Simon Helberg", "Mayim Bialik", "Jim Parsons"], "Mayim Bialik"),
+    new Question("How old sheldon was when he started his college?", ["7", "11", "13", "15"], "11")
+];
+}
+else if(currentGenre === string3){
+ questions = [
+    new Question("Who has nickname Pappu?", ["Narendra Modi", "Rahul Gandhi","Baba Ramdev", "Yogi Adityanath"], "Rahul Gandhi"),
+    new Question("Who is current governor of Reserve Bank of India?", ["Raghuram Rajan", "Urjit Patel", "Shantikanta Das", "Arun Jaitley"], "Shantikanta Das"),
+    new Question("What is NDA with respect to Indian Politics?", ["Non Disclosure Agreement", "National Democretic Alliance","National Defence Academy", "Narendra Damodardas Modi"], "National Democretic Alliance"),
+    new Question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
+    new Question("Who is president of Bahujan Samaj Party?", ["Mamta Banerjee", "Lalu Yadav", "Mayavati", "Kinjal Dave"], "Mayavati")
+];
+}
 var quiz = new Quiz(questions);
 populate();
--- form.js
+++ form.js
@@ -1,5 +1,18 @@
 //<!--Created by Meet Patel (201801415). All rights reserved :P -->
 
+function setGenreComp(){
+    localStorage.setItem("genreSelected", "Computer");
+    document.getElementById("dropButton").innerHTML = "Selected Genre: Computer";
+}
+function setGenreBBT(){
+    localStorage.setItem("genreSelected", "BigBangTheory");
+    document.getElementById("dropButton").innerHTML = "Selected Genre: Big Bang Theory TV Series";
+}
+function setGenrePolitics(){
+    localStorage.setItem("genreSelected", "Politics");
+    document.getElementById("dropButton").innerHTML = "Selected Genre: Indian Politics";
+}
+
 function getDetails(){
     var x = document.getElementById("form1");
     var y = x.elements["name"].value;
@@ -11,7 +24,7 @@ function getDetails(){
         document.getElementById("para1").innerHTML="Hello "+y+"!<br>"+"Please wait till the questions are loaded.";
         localStorage.setItem("storedName", y);
         javascript:window.location='quizPage.html';
-        startTheQuiz();
+        /* startTheQuiz(); */
     }
 }
 
@@ -28,3 +41,21 @@ function showScores() {//added again
     var element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 };
+
+
+function onclickDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
